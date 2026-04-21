52564a23b1625d6d4a4eb0946b69851325f660e3
awei808 <1934291503@qq.com>
2026-04-21 21:03:33 +0800

    更新版本，不出意外的话，能打上v3.0的tag了

--- script/version.js
+++ script/version.js
@@ -1,7 +1,6 @@
 // 自动生成版本信息
 const VERSION = {
-    NUMBER: 'c0228ca',
-    DATE: '2026-04-20',
+    NUMBER: 'befdbf5',
+    DATE: '2026-04-21',
     DESCRIPTION: '初等变换辅助学习',
-    HEAD_HASH: 'c0228ca'
 };
